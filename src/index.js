ce3864fb47e31aa682427e62c48bf81bbb3d6612
greatHannahV <i.kasl@mail.ru>
2023-11-14 20:46:47 +0200

    added files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App(){
-//react v.18
+
 return <h1>Hello React</h1>
 }
+
+
+//react v.18
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+root.render(< React.StrictMode ><App/></React.StrictMode>) //oreder componets twice in order to find bugs
 //before v.18
-//React.render(<App/>) and remove import ReactDOM from 'react-dom/client';
+//ReactDOM.render(<App/>) and remove import ReactDOM from 'react-dom/client';
